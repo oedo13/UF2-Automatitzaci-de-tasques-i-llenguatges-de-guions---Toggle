f4a4cf841f77b3d13774b530ca1b1b12aa8f1981
oedo13 <158025160+oedo13@users.noreply.github.com>
2024-02-13 09:07:05 +0100

    Update Act3_ex5_bucle.sh

--- Act3_ex5_bucle.sh
+++ Act3_ex5_bucle.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-arguments=0
-until [ ${#arguments} == 5 ] | [ $# == 3 ]
-	do
-		if [ ${#arguments} != 5 ];then
-			echo 'Posa 3 arguments'
-			read -p 'Arguments: ' arguments
-		else
-			break
-		fi
-	done
+nombres=0
+
+until [ ${#nombres} == 5 ] | [ $# == 3 ];do
+                if [ ${#nombres} != 5 ];then
+                        echo 'Introdueix 3 nombres:'
+                        read -p 'Nombres: ' nombres
+                else
+                        break
+                fi
+        done
