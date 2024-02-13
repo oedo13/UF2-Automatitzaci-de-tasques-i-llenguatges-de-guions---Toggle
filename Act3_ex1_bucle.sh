a7d8a25a917a556456eccc7c99a9efb01039c67a
oedo13 <158025160+oedo13@users.noreply.github.com>
2024-02-13 08:54:37 +0100

    Update Act3_ex1_bucle.sh

--- Act3_ex1_bucle.sh
+++ Act3_ex1_bucle.sh
@@ -1,1 +1,23 @@
+#!/bin/bash
+read -p "Escriu el fitxer que vols eliminar comentaris: " arxiu
+for linia in $(cat $arxiu)
+do
+        sed -e '/^#/d' <$arxiu>no_comentaris.txt
+done
+cat no_comentaris.txt
 
+read -p "Escriu la paraula que vols buscar: " paraula
+
+for para in $(cat no_comentaris.txt); do
+        if [ $paraula == $para ];then
+                hola=1
+        fi
+done
+if [ $hola == 1 ];then
+        echo "Exist"
+else
+        echo "No"
+fi
+read -p "Escriu la frase que afegurem al final del arxiu: " para
+echo $para >> no_comentaris.txt
+cat no_comentaris.txt
