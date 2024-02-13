67374656666b14ccc8289ca8d7e7ddaae052d041
oedo13 <158025160+oedo13@users.noreply.github.com>
2024-02-13 08:55:51 +0100

    Update Act3_ex3_bucle.sh

--- Act3_ex3_bucle.sh
+++ Act3_ex3_bucle.sh
@@ -1,1 +1,15 @@
+read -p "Escriu el nom del arxiu: " arx
+read -p "Escriu una paraula a inserir: " para
+	if [ -e $arx ]
+	then
+		echo "L'arxiu existeix"
+	else
+		echo "L'arxiu no existeix per tant és creara"
+	fi
+while [ $para != ":>" ]
+do
+	echo "$para" >> $arx
+	read -p "Escriu una paraula a inserir: " para
+done
+cat $arx
 
