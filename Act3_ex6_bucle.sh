b6a17e1071cea8622eb816c4eecadc8372397529
oedo13 <158025160+oedo13@users.noreply.github.com>
2024-02-13 09:08:39 +0100

    Update Act3_ex6_bucle.sh

--- Act3_ex6_bucle.sh
+++ Act3_ex6_bucle.sh
@@ -1,1 +1,27 @@
+#!/bin/bash
+read -p 'Ruta: ' ruta
+if [ -x $ruta ]
+then 
+	echo "Té permisos d'execució."
+fi
+if [ -r $ruta ]
+then
+	echo "Té permisos de lectura."
+fi
+if [ -w $ruta ]
+then
+	echo "Té permisos d'escriptura."
+fi
 
+ls -l $ruta > ls_rutes.txt
+wc -l ls_rutes.txt
+ls -d */  > num_directoris.txt
+wc -l num_directoris.txt
+#cat ls_rutes.txt
+directoris = 0
+arxius = 0
+while IFS= read -r linea
+do
+	echo $linea
+done < ls_rutes.txt
+echo $directoris
