5f389b76a04fbbf85c3b298545be6ed74131e4c6
oedo13 <158025160+oedo13@users.noreply.github.com>
2024-02-13 09:03:00 +0100

    Update Act3_ex5_bucle.sh

--- Act3_ex5_bucle.sh
+++ Act3_ex5_bucle.sh
@@ -1,1 +1,11 @@
-
+#!/bin/bash
+arguments=0
+until [ ${#arguments} == 5 ] | [ $# == 3 ]
+	do
+		if [ ${#arguments} != 5 ];then
+			echo 'Posa 3 arguments'
+			read -p 'Arguments: ' arguments
+		else
+			break
+		fi
+	done
